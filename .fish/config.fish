bbbb92f16bba8055253a5b83e25da34b3c27c060
disVill <konokou2000@gmail.com>
2022-04-23 10:27:47 +0900

    add config.fish

---
+++ .fish/config.fish
@@ -0,0 +1,12 @@
+set -g theme_display_git_untracked yes
+set -g theme_display_git_default_branch yes
+set -g theme_display_user yes
+set -g theme_nerd_fonts yes
+set -g theme_show_exit_status yes
+set -g theme_color_scheme dracula
+set -g theme_newline_cursor yes
+set -U FZF_LEGACY_KEYBINDINGS 0
+
+figlet Hello disVill ! | lolcat
+
+source /usr/local/opt/asdf/libexec/asdf.fish
